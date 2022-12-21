cb508af6b6686d927f23f2063e982e41ce692e65
aksonai <fatjongerra@gmail.com>
2022-12-21 23:33:30 +0100

    [ADD] Solution to R-1.3

--- ch_01/R_01.py
+++ ch_01/R_01.py
@@ -1,3 +1,6 @@
+from typing import List, Tuple
+
+
 # R-1.1 Write a short Python function, is_multiple(n, m), that takes two integer
 # values and returns True if n is a multiple of m, that is, n = mi for some
 # integer i, and False otherwise.
@@ -12,3 +15,20 @@ def is_multiple(n: int, m: int) -> bool:
 
 def is_even(k: int) -> bool:
     return bin(k)[-1] == '0'
+
+
+# R-1.3 Write a short Python function, minmax(data), that takes a sequence of
+# one or more numbers, and returns the smallest and largest numbers, in the
+# form of a tuple of length two. Do not use the built-in functions min or
+# max in implementing your solution.
+
+def minmax(data: List[int]) -> Tuple[int, int]:
+    min_val = data[0]
+    max_val = data[0]
+    for x in data:
+        if x < min_val:
+            min_val = x
+        elif x > max_val:
+            max_val = x
+
+    return (min_val, max_val)
