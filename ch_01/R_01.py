8062c6a6d9be1788d51f90f10d93053c8efb976a
aksonai <fatjongerra@gmail.com>
2022-12-23 10:10:37 +0100

    [ADD] solution to R-1.8

--- ch_01/R_01.py
+++ ch_01/R_01.py
@@ -67,3 +67,15 @@ def sum_square_odd(n: int) -> int:
 
 def sum_square_odd2(n: int) -> int:
     return sum([x**2 for x in range(n) if x % 2])
+
+
+# R-1.8 Python allows negative integers to be used as indices into a sequence,
+# such as a string. If string s has length n, and expression s[k] is used for in-
+# dex −n ≤ k < 0, what is the equivalent index j ≥ 0 such that s[j] references
+# the same element?
+
+n = 10
+k = -3
+A = list(range(n))
+print(A[k])
+print(A[k+n])
