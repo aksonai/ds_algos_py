3a377cd04eb9766106d07c8a6437327a4f6b8049
aksonai <fatjongerra@gmail.com>
2023-01-11 19:43:18 +0100

    [ADD] solution to C-1.28

--- ch_01/C_01_27_28.py
+++ ch_01/C_01_27_28.py
@@ -17,3 +17,16 @@ def factors(n):
         yield k
     while larger_factors:
         yield larger_factors.pop()
+
+
+# C-1.28 For the special case of p = 2, this results in the traditional Euclidean
+# norm, which represents the length of the vector. For example, the Euclidean 
+# norm of a two-dimensional vector with coordinates (4, 3) has a Euclidean norm 
+# of 5. Give an implementation of a function named norm such that norm(v, p) 
+# returns the p-norm value of v and norm(v) returns the Euclidean norm of v. 
+# You may assume that v is a list of numbers.
+from typing import List
+
+
+def norm(v: List[int], p: int) -> int:
+    return int(sum([x ** p for x in v]) ** (1/p))
